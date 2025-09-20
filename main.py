c6546ad3c37e6c4ea30095c6b813899702f289f0
User <mihey.kashirin@icloud.com>
2025-09-20 21:28:02 +0300

    2

--- main.py
+++ main.py
@@ -1,16 +1,54 @@
-# This is a sample Python script.
+import secrets
+import string
+from typing import Annotated
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from starlette.responses import RedirectResponse
+import json
+import aiofiles
+import os
+import uvicorn
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = FastAPI()
+
+app.mount('/static', StaticFiles(directory='static'), name='static')
+templates = Jinja2Templates(directory='templates')
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@app.get('/', response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse('mainpage.html', {"request": request})
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@app.get('/{short_url}')
+async def short_url_handler(short_url: str):
+    async with aiofiles.open('urls.json', mode='r') as f:
+        content = await f.read()
+        urls_data = json.loads(content) if content else {}
+    longurl = urls_data.get(short_url)
+    if longurl:
+        return RedirectResponse(longurl)
+    return {"error": "Not found"}
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+@app.post('/')
+async def create_url(longurl: Annotated[str, Form()]):
+    alphabet = string.ascii_letters + string.digits
+    short_url = ''.join(secrets.choice(alphabet) for _ in range(6))
+    if not os.path.exists('urls.json'):
+        urls_data = {}
+    else:
+        async with aiofiles.open('urls.json', mode='r') as f:
+            content = await f.read()
+            urls_data = json.loads(content) if content else {}
+    urls_data[short_url] = longurl
+    async with aiofiles.open('urls.json', mode='w') as f:
+        await f.write(json.dumps(urls_data, indent=4))
+    return {"short_url": short_url, "longurl": longurl}
+
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
