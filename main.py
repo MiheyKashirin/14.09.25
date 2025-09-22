b0947d236210abb851e71712c18656c3007ec12e
User <mihey.kashirin@icloud.com>
2025-09-22 22:32:57 +0300

    2

--- main.py
+++ main.py
@@ -10,11 +10,21 @@ import json
 import aiofiles
 import os
 import uvicorn
+import motor.motor_asyncio
+
 
 app = FastAPI()
 
 app.mount('/static', StaticFiles(directory='static'), name='static')
 templates = Jinja2Templates(directory='templates')
+
+mongo_host = os.environ.get('MONGO_HOST', 'localhost')
+mongo_client = motor.motor_asyncio.AsyncIOMotorClient(f'mongodb://admin:password@{mongo_host}:27017')
+db = mongo_client.shortener
+urls_collection = db.urls
+
+
+
 
 
 @app.get('/', response_class=HTMLResponse)
@@ -22,32 +32,27 @@ async def root(request: Request):
     return templates.TemplateResponse('mainpage.html', {"request": request})
 
 
+from fastapi.responses import RedirectResponse
+
+
 @app.get('/{short_url}')
 async def short_url_handler(short_url: str):
-    async with aiofiles.open('urls.json', mode='r') as f:
-        content = await f.read()
-        urls_data = json.loads(content) if content else {}
-    longurl = urls_data.get(short_url)
-    if longurl:
-        return RedirectResponse(longurl)
-    return {"error": "Not found"}
+    urls_data = await urls_collection.find_one({'short_url': short_url})
+    if urls_data:
+        await urls_collection.update_one(
+            {'short_url': short_url},
+            {'$inc': {'clicks': 1}}
+        )
+        return RedirectResponse(urls_data['long_url'])
+    return {"error": "Ссылка не найдена"}
 
 
 @app.post('/')
 async def create_url(longurl: Annotated[str, Form()]):
     alphabet = string.ascii_letters + string.digits
     short_url = ''.join(secrets.choice(alphabet) for _ in range(6))
-    if not os.path.exists('urls.json'):
-        urls_data = {}
-    else:
-        async with aiofiles.open('urls.json', mode='r') as f:
-            content = await f.read()
-            urls_data = json.loads(content) if content else {}
-    urls_data[short_url] = longurl
-    async with aiofiles.open('urls.json', mode='w') as f:
-        await f.write(json.dumps(urls_data, indent=4))
-    return {"short_url": short_url, "longurl": longurl}
-
+    await urls_collection.insert_one({'short_url': short_url, 'long_url': longurl, 'clicks': 0})
+    return {"short_url": short_url, "long_url": longurl}
 
 
 if __name__ == "__main__":
